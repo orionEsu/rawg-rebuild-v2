3581bce349b9413fcb9b3380231de26690e266b8
Esu Gabriel <orionesu.dev@gmail.com>
2023-05-23 13:01:04 +0100

    implemented theme switching

---
+++ src/theme.js
@@ -0,0 +1,29 @@
+// 1. import `extendTheme` function
+import { extendTheme } from '@chakra-ui/react';
+
+// 2. Add your color mode config
+const config = {
+	initialColorMode: 'dark',
+	useSystemColorMode: true,
+};
+
+// 3. extend the theme
+const theme = extendTheme({
+	config,
+	colors: {
+		gray: {
+			50: '#F9F9F9',
+			100: '#ededed',
+			200: '#d3d3d3',
+			300: '#b3b3b3',
+			400: '#a0a0a0',
+			500: '#898989',
+			600: '#6c6c6c',
+			700: '#202020',
+			800: '#121212',
+			900: '#111',
+		},
+	},
+});
+
+export default theme;
