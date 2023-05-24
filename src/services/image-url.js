af1bd88e9b9dfe5b8fcd927fb9d8180becba1cab
Esu Gabriel <orionesu.dev@gmail.com>
2023-05-24 16:21:56 +0100

    Removed dead code

--- src/services/image-url.js
+++ src/services/image-url.js
@@ -1,8 +1,6 @@
 const getCroppedUrl = (url) => {
-	console.log(url);
 	const description = 'media/';
 	const val = url.indexOf(description) + description.length;
-
 	return url.slice(0, val) + 'crop/600/400/' + url.slice(val);
 };
 
