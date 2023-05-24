16b76db0dcfc054e2dbd8295dc6efd15817b57e3
Esu Gabriel <orionesu.dev@gmail.com>
2023-05-24 16:19:37 +0100

    Implemented platform menu with icons

---
+++ src/components/PlatformSelector.jsx
@@ -0,0 +1,56 @@
+import {
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	Button,
+	Icon,
+} from '@chakra-ui/react';
+import { BsChevronDown } from 'react-icons/bs';
+import usePlatforms from '../hooks/usePlatform';
+import { iconMap } from '../data/iconMap';
+import PropTypes from 'prop-types';
+
+const PlatformSelector = ({ onSelectedPlatform, selectedPlatform }) => {
+	const { data: platforms, error } = usePlatforms();
+
+	const filteredPlatform = platforms.filter(
+		(el) => el.slug !== '3do' && el.slug !== 'neo-geo'
+	);
+
+	if (error) return null;
+
+	return (
+		<Menu>
+			<MenuButton
+				borderRadius='md'
+				borderWidth='1px'
+				as={Button}
+				rightIcon={<BsChevronDown />}
+			>
+				{selectedPlatform?.name || 'Platforms'}
+			</MenuButton>
+			<MenuList>
+				{filteredPlatform.map((platform) => (
+					<MenuItem
+						key={platform.id}
+						onClick={() => onSelectedPlatform(platform)}
+					>
+						<Icon
+							as={iconMap[platform.slug]}
+							mr={'12px'}
+						/>
+						{platform.name}
+					</MenuItem>
+				))}
+			</MenuList>
+		</Menu>
+	);
+};
+
+PlatformSelector.propTypes = {
+	onSelectedPlatform: PropTypes.func,
+	selectedPlatform: PropTypes.object,
+};
+
+export default PlatformSelector;
