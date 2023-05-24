7e3c581c0b5c98b2e7f7ab1c924526639be08a2f
Esu Gabriel <orionesu.dev@gmail.com>
2023-05-24 16:20:17 +0100

    Included Emoji in cards

---
+++ src/components/Emoji.jsx
@@ -0,0 +1,23 @@
+import { Image } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+import bullsEye from '../assets/bulls-eye.webp';
+import meh from '../assets/meh.webp';
+import thumbsUp from '../assets/thumbs-up.webp';
+
+const Emoji = ({ rating }) => {
+	if (rating < 3) return null;
+
+	const emojiMap = {
+		3: { src: meh, alt: 'meh', boxSize: '25px' },
+		4: { src: bullsEye, alt: 'recommended', boxSize: '35px' },
+		5: { src: thumbsUp, alt: 'exceptional', boxSize: '25px' },
+	};
+
+	return <Image {...emojiMap[rating]} marginTop={1}/>;
+};
+
+Emoji.propTypes = {
+	rating: PropTypes.number,
+};
+
+export default Emoji;
