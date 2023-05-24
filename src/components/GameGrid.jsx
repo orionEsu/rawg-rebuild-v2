975787891babbdfc4543708fa91f5fddb80c9af9
Esu Gabriel <orionesu.dev@gmail.com>
2023-05-24 16:17:05 +0100

    Implemented Error message when there isn't games

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -5,8 +5,8 @@ import useGames from '../hooks/useGames';
 import AlertCom from './AlertCom';
 import CardSkeleton from './CardSkeleton';
 
-const GameGrid = ({ onSelected }) => {
-	const { data: games, error, isLoading } = useGames(onSelected);
+const GameGrid = ({ onSelected, onPlatform }) => {
+	const { data: games, error, isLoading } = useGames(onSelected, onPlatform);
 
 	const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
@@ -14,20 +14,28 @@ const GameGrid = ({ onSelected }) => {
 
 	return (
 		<SimpleGrid
+			marginTop={5}
 			columns={3}
 			spacing={'25px'}
-			paddingX={'10'}
 		>
 			{isLoading && arr.map((el) => <CardSkeleton key={el} />)}
-			{games.map((game) => (
-				<GameCard game={game} key={game.id}/>
-			))}
+			{games.length !== 0 ? (
+				games.map((game) => (
+					<GameCard
+						game={game}
+						key={game.id}
+					/>
+				))
+			) : (
+				<AlertCom msg={'No Games in this platform'} />
+			)}
 		</SimpleGrid>
 	);
 };
 
 GameGrid.propTypes = {
 	onSelected: PropTypes.object,
+	onPlatform: PropTypes.object,
 };
 
 export default GameGrid;
