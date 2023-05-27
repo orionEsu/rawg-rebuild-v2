412797c2974f5cb810adf9b5fbcb440bfe1c176b
Esu Gabriel <orionesu.dev@gmail.com>
2023-05-27 13:10:25 +0100

    Rebuild navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 // import { Image } from '@chakra-ui/react';
-import { HStack } from '@chakra-ui/react';
+import { Box, Flex, HStack, Text } from '@chakra-ui/react';
 import '../index.css';
 import SwitchMode from './SwitchMode';
 import SearchInput from './SearchInput';
@@ -7,15 +7,40 @@ import SearchInput from './SearchInput';
 const NavBar = ({ onSearchEnter }) => {
 	return (
 		<>
-			<HStack
+			<Box
 				justifyContent={'space-between'}
-				mx={10}
-				py={10}
+				px={10}
+				my={6}
+				marginTop={10}
+				align={'center'}
+				display={{ md: 'flex' }}
+				alignItems={'center'}
+				gap={'40px'}
 			>
-				<div className='logo'>RAWG</div>
-				<SearchInput onSearchEnter={onSearchEnter} />
-				<SwitchMode />
-			</HStack>
+				<Text
+					className='logo'
+					fontSize={'18px'}
+					fontWeight={'extrabold'}
+					lineHeight={1}
+					letterSpacing={'5px'}
+					color={'#fff'}
+					position={'relative'}
+					display={{ md: 'block' }}
+					textAlign={{ md: 'left' }}
+					flex={0.5}
+					marginBottom={{ base: '25px', sm: '25px', md: '0' }}
+				>
+					RAWG
+				</Text>
+				<HStack
+					flex={1}
+					justifyContent={'space-between'}
+					gap={10}
+				>
+					<SearchInput onSearchEnter={onSearchEnter} />
+					<SwitchMode />
+				</HStack>
+			</Box>
 		</>
 	);
 };
