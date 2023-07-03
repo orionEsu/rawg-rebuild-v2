3e17431da91943690ab38ca2c8952007b5f31db8
Esu Gabriel <orionesu.dev@gmail.com>
2023-07-03 16:35:05 +0100

    Added react router

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './App.jsx';
+import { RouterProvider } from 'react-router-dom';
 import './index.css';
 import theme from './theme.js';
+import router from './routes';
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -21,7 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 		<ChakraProvider theme={theme}>
 			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 			<QueryClientProvider client={queryClient}>
-				<App />
+				<RouterProvider router={router} />
 				<ReactQueryDevtools />
 			</QueryClientProvider>
 		</ChakraProvider>
