c20b3a0d4c5060fcf1a4e638feb4d4dcb44434b3
Esu Gabriel <orionesu.dev@gmail.com>
2023-07-05 17:09:40 +0100

    modified component to be dynamic

--- src/components/PlatformSelector.jsx
+++ src/components/PlatformSelector.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {
 	Button,
+	Box,
 	Icon,
 	Menu,
 	MenuButton,
@@ -8,18 +10,32 @@ import {
 } from '@chakra-ui/react';
 import { BsChevronDown } from 'react-icons/bs';
 import { iconMap } from '../data/iconMap';
-import usePlatforms from '../hooks/usePlatforms';
+import useParentPlatform from '../hooks/useParentPlatform';
 import useGameQueryStore from '../store';
+import usePlatformFilter from '../hooks/usePlatformFilter';
+import { Link, useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const PlatformSelector = () => {
+	const { data, error } = useParentPlatform();
+	const filteredPlatform = data?.results.filter(
+		(el) => el.slug !== '3do' && el.slug !== 'neo-geo'
+	);
+	const sortedPlatforms = filteredPlatform?.map((el) => el.platforms.at(0));
+
+	const [extended, setExtended] = useState();
+	const { slug } = useParams();
+
+	const find = sortedPlatforms.find((platform) => platform.slug === slug);
+	useEffect(() => {
+		if (!find) setExtended(true);
+		if (find || !slug) setExtended(false);
+	}, [find]);
+
 	const platformId = useGameQueryStore((state) => state.gameQuery.platformId);
 	const setPlatformId = useGameQueryStore((state) => state.setPlatformId);
 
-	const { data: platforms, error } = usePlatforms();
-
-	const filteredPlatform = platforms?.results.filter(
-		(el) => el.slug !== '3do' && el.slug !== 'neo-geo'
-	);
+	const selectedPlatform = usePlatformFilter(platformId);
 
 	if (error) return null;
 
@@ -31,20 +47,30 @@ const PlatformSelector = () => {
 				as={Button}
 				rightIcon={<BsChevronDown />}
 			>
-				{platformId?.name || 'Platforms'}
+				{selectedPlatform?.name || 'Platforms'}
 			</MenuButton>
 			<MenuList>
-				{filteredPlatform?.map((platform) => (
-					<MenuItem
-						key={platform.id}
-						onClick={() => setPlatformId(platform.id)}
-					>
-						<Icon
-							as={iconMap[platform.slug]}
-							mr={'12px'}
-						/>
-						{platform.name}
-					</MenuItem>
+				{sortedPlatforms?.map((platform) => (
+					<Box key={platform.id}>
+						<Link
+							to={
+								extended
+									? `/games/${platform.slug}/${slug}`
+									: `/games/${platform.slug}`
+							}
+						>
+							<MenuItem
+								key={platform.id}
+								onClick={() => setPlatformId(platform.id)}
+							>
+								<Icon
+									as={iconMap[platform.slug]}
+									mr={'12px'}
+								/>
+								{platform.name}
+							</MenuItem>
+						</Link>
+					</Box>
 				))}
 			</MenuList>
 		</Menu>
