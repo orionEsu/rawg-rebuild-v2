f687b1be0143ad49806679f1778e19f3bd42fb45
Esu Gabriel <orionesu.dev@gmail.com>
2023-07-07 14:28:50 +0100

    Added new routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from './pages/Layout';
 import HomePage from './pages/HomePage';
-import GamesPage from './pages/GamesPage';
+import AllGamesPage from './pages/AllGamesPage';
 import GenresPage from './pages/GenresPage';
 import PlatformPage from './pages/PlatformPage';
 import GameTypePage from './pages/GameTypePage';
 import GameDescriptionTypePage from './pages/GameDescriptionTypePage';
+import Last30Days from './pages/Last30Days';
+import ThisWeekReleases from './components/ThisWeekReleases';
+import NextWeekReleases from './components/NextWeekReleases';
+import BOYReleases from './components/BoyReleases';
+import TopOf2022 from './pages/TopOf2022';
 
 const router = createBrowserRouter([
 	{
@@ -15,12 +20,17 @@ const router = createBrowserRouter([
 			{ index: true, element: <HomePage /> },
 			{
 				path: 'games',
-				element: <GamesPage />,
+				element: <AllGamesPage />,
 			},
 			{ path: 'games/:type/:slug', element: <GameDescriptionTypePage /> },
 			{ path: 'games/:slug', element: <GameTypePage /> },
 			{ path: 'genres', element: <GenresPage /> },
 			{ path: 'platforms', element: <PlatformPage /> },
+			{ path: 'last-30-days', element: <Last30Days /> },
+			{ path: 'this-week', element: <ThisWeekReleases /> },
+			{ path: 'next-week', element: <NextWeekReleases /> },
+			{ path: 'best-of-the-year', element: <BOYReleases /> },
+			{ path: 'top-of-2022', element: <TopOf2022 /> },
 		],
 	},
 ]);
