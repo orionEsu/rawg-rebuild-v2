27d6ccdd1e969d4d72ef3b611ada014327686fb2
orionEsu <orionesu.dev@gmail.com>
2023-07-18 19:33:29 +0100

    Updated components to show based on conditions & used state manager

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -5,21 +5,26 @@ import SortSelector from './SortSelector';
 import PlatformSelector from './PlatformSelector';
 import GameGrid from './GameGrid';
 import { useLocation } from 'react-router-dom';
+import useGameQueryStore from '../store';
 
 const Games = (props) => {
 	const { pathname } = useLocation();
 	const { data, isInitialLoading } = props.data;
+	const gameQuery = useGameQueryStore((state) => state.gameQuery);
+
 	return (
 		<>
 			{isInitialLoading && <Spinner size={'xl'} />}
 
 			<Box>
-				<GameHeading data={data?.pages?.at(0)} />
+				{!gameQuery.searchValue && (
+					<GameHeading data={data?.pages?.at(0)} />
+				)}
 				<Box mt={5}>
 					<HStack mb={8}>
 						{data && (
 							<>
-								<SortSelector />
+								{!gameQuery.searchValue && <SortSelector />}
 								{pathname !== '/' && pathname !== '/games' && (
 									<PlatformSelector />
 								)}
