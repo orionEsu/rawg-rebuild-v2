bbc5d14be6841f35ad419de4ba069847d57e2672
orionEsu <orionesu.dev@gmail.com>
2023-07-18 19:53:10 +0100

    hook to fetch suggested games

---
+++ src/hooks/useSuggested.js
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+import APIClient from '../services/api-client';
+import { hrToMs } from '../services/timeConverter';
+
+const useSuggested = (slug) => {
+	const apiClient = new APIClient(`games/${slug}/game-series`);
+	return useQuery({
+		queryKey: [`otherGameIn-${slug}-series`],
+		queryFn: () => apiClient.get(),
+		staleTime: hrToMs(24),
+		keepPreviousData: true,
+	});
+};
+
+export default useSuggested;
