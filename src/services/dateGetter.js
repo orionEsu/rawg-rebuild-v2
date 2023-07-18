5d552c0fa1f616312300008b6925f9b2cdcd304d
orionEsu <orionesu.dev@gmail.com>
2023-07-18 19:55:22 +0100

    updated return value from padnum function

--- src/services/dateGetter.js
+++ src/services/dateGetter.js
@@ -3,17 +3,15 @@ import padNum from './padNum';
 export default () => {
 	const today = new Date();
 	const year = today.getFullYear();
-	const { date, month } = padNum(today.getDate() + 1, today.getMonth() + 1);
+	const date = padNum(today.getDate() + 1);
+	const month = padNum(today.getMonth() + 1);
 
 	const day = today.getDay();
 	const lastDayOfTheMonth = new Date(year, month, 0).getDate();
 
 	const startDate = new Date();
 	startDate.setDate(today.getDate() - 30); // 30 days ago
-	const { date: sdm, month: startDateMonth } = padNum(
-		0,
-		startDate.getMonth() + 1
-	);
+	const startDateMonth = padNum(startDate.getMonth() + 1);
 
 	return {
 		today,
