9e55bf00f07b0d3bdec75a1bba52e18a3ac62f3b
orionEsu <orionesu.dev@gmail.com>
2023-07-18 19:57:25 +0100

    added reusable component

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -1,9 +1,8 @@
-import { Button, Heading, Image, List, ListItem } from '@chakra-ui/react';
-import getCroppedUrl from '../services/image-url';
-import AlertCom from './AlertCom';
-import useGenres from '../hooks/useGenres';
 import PropTypes from 'prop-types';
+import useGenres from '../hooks/useGenres';
 import useGameQueryStore from '../store';
+import AlertCom from './AlertCom';
+import DisplayList from './DisplayList';
 
 const GenreList = () => {
 	const genreId = useGameQueryStore((state) => state.gameQuery.genreId);
@@ -13,53 +12,13 @@ const GenreList = () => {
 	if (error) return <AlertCom msg={error} />;
 
 	return (
-		<>
-			<Heading
-				mb={'16px'}
-				fontSize={'24px'}
-			>
-				Genres
-			</Heading>
-			<List>
-				{genres?.results?.map((genre) => (
-					<ListItem
-						key={genre.id}
-						mb={3}
-					>
-						<Button
-							whiteSpace={'normal'}
-							textAlign={'left'}
-							variant={'link'}
-							style={{ textDecoration: 'none' }}
-							_hover={{ color: 'hsla(0,0%,100%,.4)' }}
-							onClick={() => setGenreId(genre.id)}
-							// fontWeight={
-							// 	gameQuery.allYear ||
-							// 	gameQuery.nextWeek ||
-							// 	gameQuery.thisWeek ||
-							// 	gameQuery.lastestRelease
-							// 		? 'normal'
-							// : genre.id === gameQuery?.genreId
-							// ? 'bold'
-							// : ''
-							// }
-							fontWeight={
-								genre.id === genreId ? 'bold' : 'normal'
-							}
-						>
-							<Image
-								src={getCroppedUrl(genre.image_background)}
-								boxSize={'32px'}
-								borderRadius={'5'}
-								objectFit={'cover'}
-								mr={2}
-							/>
-							{genre.name}
-						</Button>
-					</ListItem>
-				))}
-			</List>
-		</>
+		<DisplayList
+			link={'genres'}
+			heading={'Genres'}
+			data={genres}
+			set={setGenreId}
+			id={genreId}
+		/>
 	);
 };
 
