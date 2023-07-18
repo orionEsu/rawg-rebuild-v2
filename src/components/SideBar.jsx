f853dec9e454eeaa7cb59f40520d89aa42884b69
orionEsu <orionesu.dev@gmail.com>
2023-07-18 19:56:22 +0100

    Added new lists components

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,78 +2,50 @@ import GenreList from './GenreList';
 import { Button, Box } from '@chakra-ui/react';
 import { GoHome } from 'react-icons/go';
 import useGameQueryStore from '../store';
+import NewReleasesList from './NewReleasesList';
+import TopReleases from './TopReleases';
+import BrowseList from './BrowseList';
+import PlatformList from './PlatformList';
+import { Link } from 'react-router-dom';
 
 const SideBar = () => {
 	const setDefault = useGameQueryStore((state) => state.setDefault);
 
 	return (
 		<Box
-			ml={'30px'}
 			position={'sticky'}
 			minH={'100vh'}
+			paddingLeft={'40px'}
 		>
-			<Button
-				variant={'link'}
-				style={{ textDecoration: 'none' }}
-				_hover={{ color: 'hsla(0,0%,100%,.4)' }}
-				fontSize={'24px'}
-				fontWeight={'700'}
-				display={'flex'}
-				columnGap={2}
-				mt={'2rem'}
-				mb={'4rem'}
-				onClick={() => setDefault()}
+			<Link to={''}>
+				<Button
+					variant={'link'}
+					style={{ textDecoration: 'none' }}
+					_hover={{ color: 'hsla(0,0%,100%,.4)' }}
+					fontSize={'24px'}
+					fontWeight={'700'}
+					display={'flex'}
+					columnGap={2}
+					mt={'2rem'}
+					mb={'4rem'}
+					color={'gray.50'}
+					onClick={() => setDefault()}
+				>
+					<GoHome />
+					Home
+				</Button>
+			</Link>
+
+			<NewReleasesList />
+			<TopReleases />
+			<BrowseList />
+
+			<Box
+				mb={'30px'}
+				mt={'30px'}
 			>
-				<GoHome />
-				Home
-			</Button>
-			{/* <ReleasesList
-							onSelectLast30Days={() =>
-								setGameQuery({
-									...gameQuery,
-									lastestRelease: true,
-									thisWeek: false,
-									nextWeek: false,
-									allYear: false,
-									platformId: {},
-									orderedValue: '',
-								})
-							}
-							onSelectThisWeek={() =>
-								setGameQuery({
-									...gameQuery,
-									thisWeek: true,
-									lastestRelease: false,
-									nextWeek: false,
-									allYear: false,
-									platformId: {},
-									orderedValue: '',
-								})
-							}
-							onSelectNextWeek={() =>
-								setGameQuery({
-									...gameQuery,
-									nextWeek: true,
-									thisWeek: false,
-									lastestRelease: false,
-									allYear: false,
-									platformId: {},
-									orderedValue: '',
-								})
-							}
-							onSelectAllYear={() =>
-								setGameQuery({
-									...gameQuery,
-									allYear: true,
-									nextWeek: false,
-									thisWeek: false,
-									lastestRelease: false,
-									platformId: {},
-									orderedValue: '',
-								})
-							}
-							gameQuery={gameQuery}
-						/> */}
+				<PlatformList />
+			</Box>
 			<GenreList />
 		</Box>
 	);
