1f0941e306412f55285fc987c5006a03cde851bb
orionEsu <orionesu.dev@gmail.com>
2023-07-18 19:34:42 +0100

    Modified styles and removed switch component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,44 +1,34 @@
-import { Box, HStack, Text } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 import '../index.css';
 import SearchInput from './SearchInput';
-import SwitchMode from './SwitchMode';
 
 const NavBar = () => {
 	return (
 		<>
 			<Box
-				justifyContent={'space-between'}
 				px={10}
 				my={6}
 				marginTop={10}
-				align={'center'}
-				display={{ md: 'flex' }}
+				display={'flex'}
+				alignContent={'center'}
 				alignItems={'center'}
-				gap={'40px'}
+				justifyContent={'space-between'}
+				flexDirection={{
+					base: 'column',
+					lg: 'row',
+				}}
+				rowGap={5}
 			>
 				<Text
-					className='logo'
 					fontSize={'18px'}
 					fontWeight={'extrabold'}
 					lineHeight={1}
 					letterSpacing={'5px'}
 					color={'#fff'}
-					position={'relative'}
-					display={{ md: 'block' }}
-					textAlign={{ md: 'left' }}
-					flex={0.5}
-					marginBottom={{ base: '25px', sm: '25px', md: '0' }}
 				>
 					RAWG
 				</Text>
-				<HStack
-					flex={1}
-					justifyContent={'space-between'}
-					gap={10}
-				>
-					<SearchInput />
-					<SwitchMode />
-				</HStack>
+				<SearchInput />
 			</Box>
 		</>
 	);
