4d03eed7909e4b0d5e1ed7180a1c1a355f878c4c
orionEsu <orionesu.dev@gmail.com>
2023-07-18 19:53:59 +0100

    hook to fetch game screenshots

---
+++ src/hooks/useScreenshots.js
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+import APIClient from '../services/api-client';
+import { hrToMs } from '../services/timeConverter';
+
+const useScreenshots = (slug) => {
+	const apiClient = new APIClient(`games/${slug}/screenshots`);
+	return useQuery({
+		queryKey: ['screenshots'],
+		queryFn: () => apiClient.get(),
+		staleTime: hrToMs(24),
+		keepPreviousData: true,
+	});
+};
+
+export default useScreenshots;
