0db0fd880db1fcd5fca1ceb51214ce044380feff
orionEsu <orionesu.dev@gmail.com>
2023-07-18 20:01:46 +0100

    Page to show best games of the year

--- src/components/BoyReleases.jsx
+++ src/components/BoyReleases.jsx
@@ -1,34 +1,45 @@
-import { SimpleGrid } from '@chakra-ui/react';
-import PropTypes from 'prop-types';
-import { arr } from '../data/loadingData';
-import useThisYearsReleases from '../hooks/useThisYearsReleases';
-import AlertCom from './AlertCom';
-import CardSkeleton from './CardSkeleton';
-import GameCard from './GameCard';
+import { useEffect } from 'react';
+import useReleases from '../hooks/useReleases';
+import dateGetter from '../services/dateGetter';
+import useGameQueryStore from '../store';
+import Games from './Games';
 
-const BoyReleases = ({ gameQuery }) => {
-	const { data, error, isLoading } = useThisYearsReleases(gameQuery);
-	if (error) return <AlertCom msg={error} />;
+const BOYReleases = () => {
+	const setPlatformId = useGameQueryStore((state) => state.setPlatformId);
+	const setSortValue = useGameQueryStore((state) => state.setSortValue);
+
+	useEffect(() => {
+		setSortValue('');
+		setPlatformId('');
+	}, []);
+
+	const { year } = dateGetter();
+	const startDate = `${year}-01-01`;
+	const endDate = `${year}-12-31`;
+
+	const {
+		data,
+		error,
+		isInitialLoading,
+		isFetching,
+		isFetchingNextPage,
+		fetchNextPage,
+		hasNextPage,
+	} = useReleases('this-year-releases', startDate, endDate);
 
 	return (
-		<SimpleGrid
-			marginTop={5}
-			columns={3}
-			spacing={'25px'}
-		>
-			{isLoading && arr.map((el) => <CardSkeleton key={el} />)}
-
-			{data?.map((game) => (
-				<GameCard
-					game={game}
-					key={game.id}
-				/>
-			))}
-		</SimpleGrid>
+		<Games
+			data={{
+				data,
+				error,
+				isInitialLoading,
+				isFetching,
+				isFetchingNextPage,
+				fetchNextPage,
+				hasNextPage,
+			}}
+		/>
 	);
 };
 
-BoyReleases.propTypes = {
-	gameQuery: PropTypes.object,
-};
-export default BoyReleases;
+export default BOYReleases;
