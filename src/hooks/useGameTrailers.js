422e33687942b0cb8fac3689b0732400f5583d48
orionEsu <orionesu.dev@gmail.com>
2023-07-18 19:53:48 +0100

    hook to fetch game trailers

---
+++ src/hooks/useGameTrailers.js
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+import APIClient from '../services/api-client';
+import { hrToMs } from '../services/timeConverter';
+
+const useGameTrailers = (slug) => {
+	const apiClient = new APIClient(`games/${slug}/movies`);
+	return useQuery({
+		queryKey: [`${slug}-trailers`],
+		queryFn: () => apiClient.get(),
+		staleTime: hrToMs(24),
+		keepPreviousData: true,
+	});
+};
+
+export default useGameTrailers;
