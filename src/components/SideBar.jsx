84fea13e2f09f460aacead058e448cb9a711920c
orionEsu <orionesu.dev@gmail.com>
2023-07-29 08:45:24 +0100

    Implemented sticky sidebar feature

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,8 +7,11 @@ import TopReleases from './TopReleases';
 import BrowseList from './BrowseList';
 import PlatformList from './PlatformList';
 import { Link } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
 
 const SideBar = () => {
+	const [sidebarStyles, setSidebarStyles] = useState();
+	const sidebarContentRef = useRef(null);
 	const setDefault = useGameQueryStore((state) => state.setDefault);
 
 	const nav = document.querySelector('.navBar');
@@ -23,12 +26,42 @@ const SideBar = () => {
 	);
 	navObserver?.observe(scrollWatcher);
 
-	
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollTop = window.scrollY;
+			const sidebarContent = sidebarContentRef.current;
+			const viewPort = window.innerHeight;
+			const sidebarContentHeight =
+				sidebarContent?.getBoundingClientRect().height;
+
+			if (sidebarContent && scrollTop >= 1351.17 - viewPort) {
+				setSidebarStyles({
+					position: 'fixed',
+					transform: `translateY(-${1351.17 - viewPort}px)`,
+				});
+			} else {
+				setSidebarStyles({
+					position: '',
+					transform: `translateY(0)`,
+				});
+			}
+		};
+
+		window.addEventListener('scroll', handleScroll, { passive: true });
+
+		// return () => {
+		// 	window.removeEventListener('scroll', handleScroll);
+		// };
+	}, []);
+
 	return (
 		<Box
 			minH={'100vh'}
 			paddingLeft={'40px'}
+			// transition={'transform .1s ease'}
 			className={'sidebar-content'}
+			ref={sidebarContentRef}
+			style={sidebarStyles}
 		>
 			<Link to={''}>
 				<Button
