a61cf22f0cc5a1335ebc1226637cfb2a9c02c796
orionEsu <orionesu.dev@gmail.com>
2023-07-29 08:45:32 +0100

    404 page

--- src/pages/ErrorBoundary.jsx
+++ src/pages/ErrorBoundary.jsx
@@ -1,15 +1,68 @@
-import { useRouteError } from 'react-router-dom';
+import { Box, Button, Heading, Text } from '@chakra-ui/react';
+import { Link, useRouteError } from 'react-router-dom';
 import { isRouteErrorResponse } from 'react-router-dom';
 
 const ErrorBoundary = () => {
 	const error = useRouteError();
-	console.log(error);
 
 	if (!isRouteErrorResponse(error)) return;
 	return (
-		<div>
-			{isRouteErrorResponse(error) ? 'invalid page' : 'thrown error'}
-		</div>
+		<>
+			<Box
+				display={'flex'}
+				flexDirection={'column'}
+				paddingLeft={'13rem'}
+				height={'100vh'}
+				margin={'auto'}
+				justifyContent={'center'}
+			>
+				<Heading
+					fontSize={'200px'}
+					fontWeight={'700'}
+					marginBottom={10}
+				>
+					404
+				</Heading>
+				<Text
+					fontSize={'xl'}
+					fontWeight={'600'}
+					marginBottom={6}
+				>
+					Page not found.
+				</Text>
+				<Link to={'/'}>
+					<Button
+						boxShadow={'0 0 20px 0 hsla(0,0%,100%,.2)'}
+						backgroundColor={'#fff'}
+						color={'#000'}
+						transition={'backgroundColor .3s ease'}
+						_hover={{
+							backgroundColor: 'rgba(255,255,255, 0.7)',
+						}}
+						width={'300px'}
+						paddingBlock={6}
+						fontSize={'lg'}
+					>
+						Go Home
+					</Button>
+				</Link>
+			</Box>
+			<Box
+				backgroundColor={'transparent'}
+				backgroundImage={
+					'linear-gradient(to left, rgba(0, 0, 0, 0.7), rgb(21, 21, 21)), url("https://media.rawg.io/media/games/3e8/3e81585ecda204d4f4b80a041b069adb.jpg")'
+				}
+				position={'absolute'}
+				top={0}
+				height={'100%'}
+				width={'100%'}
+				zIndex={'-2'}
+				backgroundRepeat={'no-repeat'}
+				backgroundPosition={'center'}
+				backgroundSize={'cover'}
+			>
+			</Box>
+		</>
 	);
 };
 
