efe38fc3904ae0bf4f1db3a03e2fe62a26bc4131
orionEsu <orionesu.dev@gmail.com>
2023-08-06 16:37:48 +0100

    removed sticky sidebar feature

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,67 +1,23 @@
-import GenreList from './GenreList';
-import { Button, Box } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
 import { GoHome } from 'react-icons/go';
-import useGameQueryStore from '../store';
-import NewReleasesList from './NewReleasesList';
-import TopReleases from './TopReleases';
-import BrowseList from './BrowseList';
-import PlatformList from './PlatformList';
 import { Link } from 'react-router-dom';
-import { useEffect, useRef, useState } from 'react';
+import useGameQueryStore from '../store';
+import BrowseList from './BrowseList';
+import GenreList from './GenreList';
+import NewReleasesList from './NewReleasesList';
+import PlatformList from './PlatformList';
+import TopReleases from './TopReleases';
 
 const SideBar = () => {
-	const [sidebarStyles, setSidebarStyles] = useState();
-	const sidebarContentRef = useRef(null);
 	const setDefault = useGameQueryStore((state) => state.setDefault);
-
-	const nav = document.querySelector('.navBar');
-	const scrollWatcher = document.querySelector('.scrollWatcher');
-	const navObserver = new IntersectionObserver(
-		(entries) => {
-			nav.classList.toggle('sticking', !entries[0].isIntersecting);
-		},
-		{
-			rootMargin: '20px 0px 0px 0px',
-		}
-	);
-	navObserver?.observe(scrollWatcher);
-
-	useEffect(() => {
-		const handleScroll = () => {
-			const scrollTop = window.scrollY;
-			const sidebarContent = sidebarContentRef.current;
-			const viewPort = window.innerHeight;
-			const sidebarContentHeight =
-				sidebarContent?.getBoundingClientRect().height;
-
-			if (sidebarContent && scrollTop >= 1351.17 - viewPort + 80) {
-				setSidebarStyles({
-					position: 'fixed',
-					transform: `translateY(-${1351.17 - viewPort + 80}px)`,
-				});
-			} else {
-				setSidebarStyles({
-					position: '',
-					transform: `translateY(0)`,
-				});
-			}
-		};
-
-		window.addEventListener('scroll', handleScroll, { passive: true });
-
-		// return () => {
-		// 	window.removeEventListener('scroll', handleScroll);
-		// };
-	}, []);
-
+	
 	return (
 		<Box
 			minH={'100vh'}
 			paddingLeft={'40px'}
 			// transition={'transform .1s ease'}
-			className={'sidebar-content'}
-			ref={sidebarContentRef}
-			style={sidebarStyles}
+			// position={'sticky'}
+			// top={'100px'}
 		>
 			<Link to={''}>
 				<Button
