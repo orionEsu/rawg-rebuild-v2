1d51e1b54d69380fb62f094459d781fd699e9c22
orionEsu <orionesu.dev@gmail.com>
2023-08-06 16:24:45 +0100

    Updated search logic

--- src/components/GameSearch.jsx
+++ src/components/GameSearch.jsx
@@ -1,13 +1,21 @@
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import useGameQueryStore from '../store';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import useGames from '../hooks/useGames';
+import useGameQueryStore from '../store';
 import Games from './Games';
 
 const GameSearch = () => {
-	const { query } = useParams();
+	const [searchParams, setSearchParams] = useSearchParams();
 	const gameQuery = useGameQueryStore((state) => state.gameQuery);
 	const setSearchValue = useGameQueryStore((state) => state.setSearchValue);
+	const query = searchParams.get('query');
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!query) {
+			navigate('/', { replace: true });
+		}
+	}, []);
 
 	useEffect(() => {
 		// Only set the 'searchValue' if it's not equal to the value from the URL query parameter
