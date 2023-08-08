ea03ebc998c97f068d38b02624a409596ac101ff
orionEsu <orionesu.dev@gmail.com>
2023-08-08 19:51:17 +0100

    Updated expandable function

--- src/components/ExpandableText.jsx
+++ src/components/ExpandableText.jsx
@@ -1,34 +1,36 @@
 /* eslint-disable react/prop-types */
 import { Text, Button } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ExpandableText = ({ children }) => {
 	const [expanded, setExpanded] = useState(false);
-	const limit = 300;
+	const pathname = useParams();
 
+	useEffect(() => {
+		setExpanded(false);
+	}, [pathname]);
+
+	const limit = 300;
 	if (!children) return;
 	if (children.length <= limit) return <Text>{children}</Text>;
-
-	const summary = expanded
-		? children
-		: children.substring(0, limit) + ' ....';
+	const summary = expanded ? children : children.substring(0, limit) + '....';
 	return (
-		<Text>
+		<>
 			{summary}
 
 			<Button
-				display={expanded ? 'none' : 'inline'}
 				size='xs'
 				marginLeft={1}
 				fontWeight='bold'
-				// colorScheme='yellow'
 				backgroundColor={'gray.50'}
+				transition={'all 0.2s ease'}
 				color={'gray.900'}
 				onClick={() => setExpanded(!expanded)}
 			>
-				read more
+				{expanded ? 'show less' : 'read more'}
 			</Button>
-		</Text>
+		</>
 	);
 };
 
