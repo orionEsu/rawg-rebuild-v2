bad629b160a06401aa91e7ec6f5fbb54cd5825ab
orionEsu <orionesu.dev@gmail.com>
2023-08-08 19:51:42 +0100

    Improved game card display

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,5 +1,12 @@
 /* eslint-disable react/prop-types */
-import { Card, CardHeader, HStack, Heading, Image } from '@chakra-ui/react';
+import {
+	Card,
+	CardHeader,
+	HStack,
+	Heading,
+	Image,
+	Box,
+} from '@chakra-ui/react';
 import { forwardRef } from 'react';
 import getCroppedUrl from '../services/image-url';
 import CardIcons from './CardIcons';
@@ -9,38 +16,56 @@ import { Link } from 'react-router-dom';
 
 const GameCard = forwardRef(({ game }, ref) => {
 	const platformObj = game?.parent_platforms?.map((p) => p.platform);
-
 	return (
-		<Card
-			borderRadius={10}
-			overflow={'hidden'}
-			ref={ref}
-			boxShadow={'0 10px 20px 0 rgba(0,0,0,.07)'}
-		>
-			<Image src={getCroppedUrl(game.background_image)} />
+		<Link to={`/games/${game.slug}/details`}>
+			<Card
+				borderRadius={10}
+				overflow={'hidden'}
+				ref={ref}
+				boxShadow={'0 10px 20px 0 rgba(0,0,0,.07)'}
+				transition={'transform .2s ease'}
+				_hover={{
+					boxShadow: '0 10px 20px 0 rgba(0,0,0,.07)',
+					transform: 'translateY(-5px)',
+				}}
+				cursor={'pointer'}
+			>
+				<Image src={getCroppedUrl(game.background_image)} />
 
-			<CardHeader alignContent={'center'}>
-				<HStack
-					mb={3}
-					justifyContent={'space-between'}
-				>
-					<HStack>
-						<CardIcons platform={platformObj} />
-					</HStack>
-					<CriticScore score={game.metacritic} />
-				</HStack>
-
-				<Link to={`/games/${game.slug}/details`}>
-					<Heading
-						fontSize={'24px'}
-						color={'gray.50'}
+				<CardHeader alignContent={'center'}>
+					<HStack
+						mb={3}
+						justifyContent={'space-between'}
+						gap={'1rem'}
 					>
-						{game.name}
+						<HStack
+							gap={'.6rem'}
+							wrap={'wrap'}
+						>
+							<CardIcons platform={platformObj} />
+						</HStack>
+						<CriticScore score={game.metacritic} />
+					</HStack>
+
+					<Box
+						display={'inline-flex'}
+						alignItems={'flex-end'}
+						columnGap={'6px'}
+					>
+						<Heading
+							fontSize={'24px'}
+							color={'gray.50'}
+							marginRight={'auto'}
+							display={'inline-flex'}
+							alignItems={'center'}
+						>
+							{game.name}
+						</Heading>
 						<Emoji rating={game.rating_top} />
-					</Heading>
-				</Link>
-			</CardHeader>
-		</Card>
+					</Box>
+				</CardHeader>
+			</Card>
+		</Link>
 	);
 });
 
