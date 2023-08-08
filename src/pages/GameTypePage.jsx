d49c99d5120597bfe6a6b9f4cb0a9980ecfe4820
orionEsu <orionesu.dev@gmail.com>
2023-08-08 19:45:06 +0100

    set state

--- src/pages/GameTypePage.jsx
+++ src/pages/GameTypePage.jsx
@@ -3,9 +3,12 @@ import { useParams } from 'react-router-dom';
 import Games from '../components/Games';
 import useGenericFetch from '../hooks/useGenericFetch';
 import useFindPlatformBySlug from '../hooks/useFindPlatformBySlug';
+import genres from '../data/genres';
 import useGameQueryStore from '../store';
 
 const GameTypePage = () => {
+	const gameQuery = useGameQueryStore((state) => state.gameQuery);
+	const setGenreId = useGameQueryStore((state) => state.setGenreId);
 	const setPlatformId = useGameQueryStore((state) => state.setPlatformId);
 	const setSortValue = useGameQueryStore((state) => state.setSortValue);
 	const params = useParams();
@@ -13,8 +16,13 @@ const GameTypePage = () => {
 	let type;
 
 	const selectedPlatform = useFindPlatformBySlug(slug);
+	const filteredGenre = genres?.results.filter((el) => el.slug === slug);
 
 	if (!selectedPlatform) type = 'genres';
+
+	if (!selectedPlatform && !gameQuery.genreId) {
+		setGenreId(filteredGenre?.[0].id);
+	}
 
 	if (selectedPlatform) {
 		type = 'parent_platforms';
@@ -30,13 +38,16 @@ const GameTypePage = () => {
 		fetchNextPage,
 		hasNextPage,
 	} = useGenericFetch(slug, type, `${slug}Games`);
-
 	useEffect(() => {
 		if (data) {
 			setSortValue('');
 
 			if (type === 'genres' && isNaN(slug)) {
 				setPlatformId('');
+			}
+
+			if (type !== 'genres') {
+				setGenreId('');
 			}
 		}
 	}, [type, slug]);
