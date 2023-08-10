32842da47b6bc158c43ea6951ae92762d7c3c322
orionEsu <orionesu.dev@gmail.com>
2023-08-10 09:04:23 +0100

    replaced button with heading

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 import { GoHome } from 'react-icons/go';
 import { Link } from 'react-router-dom';
 import useGameQueryStore from '../store';
@@ -10,33 +10,31 @@ import TopReleases from './TopReleases';
 
 const SideBar = () => {
 	const setDefault = useGameQueryStore((state) => state.setDefault);
-	
+
 	return (
 		<Box
 			minH={'100vh'}
 			paddingLeft={'40px'}
-			// transition={'transform .1s ease'}
-			// position={'sticky'}
-			// top={'100px'}
 		>
-			<Link to={''}>
-				<Button
-					variant={'link'}
-					style={{ textDecoration: 'none' }}
-					_hover={{ color: 'hsla(0,0%,100%,.4)' }}
-					fontSize={'24px'}
-					fontWeight={'700'}
-					display={'flex'}
-					columnGap={2}
-					mt={'2rem'}
-					mb={'4rem'}
-					color={'gray.50'}
-					onClick={() => setDefault()}
+			<Heading
+				transition={'color .2s ease'}
+				_hover={{ color: 'hsla(0,0%,100%,.4)' }}
+				fontSize={'24px'}
+				fontWeight={'700'}
+				mt={'2rem'}
+				mb={'4rem'}
+				color={'gray.50'}
+				onClick={() => setDefault()}
+				maxW={'fit-content'}
+			>
+				<Link
+					to={''}
+					style={{ display: 'flex', columnGap: '.4rem' }}
 				>
 					<GoHome />
 					Home
-				</Button>
-			</Link>
+				</Link>
+			</Heading>
 
 			<NewReleasesList />
 			<TopReleases />
