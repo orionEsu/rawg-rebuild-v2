6e08efa50fa2365e1d15e04795f928e060038373
orionEsu <orionesu.dev@gmail.com>
2023-08-14 19:35:04 +0100

    filtered sort array based on url

--- src/components/SortSelector.jsx
+++ src/components/SortSelector.jsx
@@ -1,10 +1,13 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import { BsChevronDown } from 'react-icons/bs';
 import useGameQueryStore from '../store';
+import { useLocation } from 'react-router-dom';
 
 const SortSelector = () => {
+	const { pathname } = useLocation();
 	const sortValue = useGameQueryStore((state) => state.gameQuery.sortValue);
 	const setSortValue = useGameQueryStore((state) => state.setSortValue);
+	const previousDate = new Date().getFullYear() - 1;
 
 	const sortOrders = [
 		{ value: '', label: 'Popularity' },
@@ -15,8 +18,23 @@ const SortSelector = () => {
 		{ value: '-metacritic', label: 'Metacritic' },
 	];
 
-	const currentOrder = sortOrders.find((el) => el.value === sortValue);
+	const checkPath = () => {
+		if (
+			pathname === '/' ||
+			pathname === '/last-30-days' ||
+			pathname === '/this-week' ||
+			pathname === '/next-week' || 
+			pathname === '/best-of-the-year' || 
+			pathname === `/top-of-${previousDate}`
+		)
+			return sortOrders.filter(
+				(el) => el.value !== '-added' && el.value !== '-released'
+			);
+		return sortOrders;
+	};
 
+	const currentOrder = sortOrders.find((el) => el.value === sortValue);
+	
 	return (
 		<Menu>
 			<MenuButton
@@ -28,7 +46,7 @@ const SortSelector = () => {
 				Order By: {currentOrder?.label || 'Popularity'}
 			</MenuButton>
 			<MenuList>
-				{sortOrders.map((order) => (
+				{checkPath().map((order) => (
 					<MenuItem
 						key={order.value}
 						value={order.value}
