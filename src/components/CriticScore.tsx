323f93d2ec082c167744697497226c363b45888f
orionEsu <orionesu.dev@gmail.com>
2023-12-07 21:06:11 +0100

    Added types to component

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -1,24 +1,24 @@
 import { Badge } from '@chakra-ui/react';
-import PropTypes from 'prop-types';
 
-const CriticScore = ({ score }) => {
-	let color = score > 70 ? '#6dc849' : score > 60 ? 'yellow' : 'white';
+type CriticScoreProps = {
+	score: number;
+};
+
+const CriticScore = ({ score }: CriticScoreProps) => {
+	let color = score > 70 ? "#6dc849" : score > 60 ? "yellow" : "white";
 
 	return (
 		<Badge
 			color={color}
 			px={2}
 			py={1}
-			fontWeight={'900'}
-			fontSize={'md'}
+			fontWeight={"900"}
+			fontSize={"md"}
 			borderRadius={5}
 		>
-			{score || 'NR'}
+			{score || "NR"}
 		</Badge>
 	);
 };
 
-CriticScore.propTypes = {
-	score: PropTypes.number,
-};
 export default CriticScore;
